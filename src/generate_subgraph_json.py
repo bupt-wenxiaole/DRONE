0d60c60682bd9d5849ae74f6d0cd6267c8627a57
bupt-wenxiaole <506539136@qq.com>
2017-11-22 17:44:48 +0800

    generate Fi and Fi.O and Fi.I finish

--- src/generate_subgraph_json.py
+++ src/generate_subgraph_json.py
@@ -13,16 +13,35 @@ with open("/home/xwen/GRAPE/test_data/small_metis_graph", "r") as r1 , open("/ho
     subGraphJsonMap = {}
     for i in range(partition_num):
         subGraph = "Graph" + str(i)
+        subGraphFI = subGraph + "F.I"
+        subGraphFO = subGraph + "F.O"
         subGraphJsonMap[subGraph] = {}
+        subGraphJsonMap[subGraphFI] = {}
+        subGraphJsonMap[subGraphFO] = {}
     for j in range(len(r1_lines)):
         nodeIndex = j + 1
         nodeSubGraph = "Graph" + str(r2_lines[j])
+        nodeSubGraphFI = nodeSubGraph + "F.I"
+        nodeSubGraphFO = nodeSubGraph + "F.O"
         subGraphJsonMap[nodeSubGraph][str(nodeIndex)] = {}
+        subGraphJsonMap[nodeSubGraphFI][str(nodeIndex)] = {}
         for k in range(0, len(r1_lines[j]), 2):
+            # K means another node in pair 
             if r2_lines[r1_lines[j][k] - 1] == r2_lines[j]:
                 #this two nodes belongs to one subgraph
-                subGraphJsonMap[nodeSubGraph][str(nodeIndex)][str(r1_lines[j][k])] = str(r1_lines[j][k+1])
-with open("subgraph_json", "w") as w:
+                subGraphJsonMap[nodeSubGraph][str(nodeIndex)][str(r1_lines[j][k])] = r1_lines[j][k+1]
+            else:
+                #this two nodes belongs to different subgraphs, Border
+                #   first fills in Fi.I
+                #   then  fills in Fi.O
+                subGraphJsonMap[nodeSubGraphFI][str(nodeIndex)][str(r1_lines[j][k])] = str(r1_lines[j][k+1]) + " " + str(r2_lines[r1_lines[j][k] - 1])
+                if str(r1_lines[j][k]) in subGraphJsonMap[nodeSubGraphFO]:
+                    subGraphJsonMap[nodeSubGraphFO][str(r1_lines[j][k])][str(nodeIndex)] = str(r1_lines[j][k+1]) + " " + str(r2_lines[r1_lines[j][k] - 1])
+                else:
+                    subGraphJsonMap[nodeSubGraphFO][str(r1_lines[j][k])] = {}
+                    subGraphJsonMap[nodeSubGraphFO][str(r1_lines[j][k])][str(nodeIndex)] = str(r1_lines[j][k+1]) + " " + str(r2_lines[r1_lines[j][k] - 1])
+
+with open("/home/xwen/GRAPE/src/subgraph_json", "w") as w:
     w.write(json.dumps(subGraphJsonMap, sort_keys=True, indent=4, separators=(',', ': ')))
         
 
