38cc0143847a0309b81e550ccdda992feed001e2
zpltys <gzpltys@gmail.com>
2017-12-11 11:32:17 +0800

    SSSP local test pass

---
+++ src/algorithm/SSSP.go
@@ -0,0 +1,245 @@
+package algorithm
+
+import (
+	"graph"
+	"container/heap"
+	//"fmt"
+)
+// for more information about this implement of priority queue,
+// you can reference https://golang.org/pkg/container/heap/
+// we use Pair for store distance message associated with node ID
+type Pair struct {
+	NodeId graph.ID
+	Distance int64
+}
+
+type PriorityQueue []*Pair
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].Distance < pq[j].Distance
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*Pair))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	p := old[n-1]
+	*pq = old[0 : n-1]
+	return p
+}
+
+
+// store the weight of edge connected to DstID
+type BoundMsg struct {
+	DstId graph.ID
+	RouteLen int64
+}
+
+// in the result -- routeTable, routeTable[i] stores a list of BoundMsg,
+// routeTable[node_i][j].RouteLen is the weight of edge: node_i -> routeTable[node_i][j].DstID
+func GenerateRouteTable(FO map[graph.ID][]graph.RouteMsg) map[graph.ID][]*BoundMsg {
+	routeTable := make(map[graph.ID][]*BoundMsg)
+	for fo, msgs := range FO {
+		for _, msg := range msgs {
+			srcId := msg.RelatedId()
+			if _, ok := routeTable[srcId]; !ok {
+				routeTable[srcId] = make([]*BoundMsg, 0)
+			}
+
+			nowMsg := &BoundMsg{
+				DstId:    fo,
+				RouteLen: int64(msg.RelatedWgt()),
+			}
+			routeTable[srcId] = append(routeTable[srcId], nowMsg)
+		}
+	}
+	return routeTable
+}
+
+func combine(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// this function is used for combine transfer message
+func SSSP_aggregateMsg(oriMsg []*Pair) []*Pair {
+	msg := make([]*Pair, 0)
+	msgMap := make(map[graph.ID]int64)
+	for _, m := range oriMsg {
+		if beforeVal, ok := msgMap[m.NodeId]; ok {
+			msgMap[m.NodeId] = combine(beforeVal, m.Distance)
+		} else {
+			msgMap[m.NodeId] = m.Distance
+		}
+	}
+
+	for id, dis := range msgMap {
+		msg = append(msg, &Pair{NodeId:id, Distance: dis,})
+	}
+	return msg
+}
+
+// g is the graph structure of graph
+// distance stores distance from startId to this nodeId, and is initialed to be infinite
+// exchangeMsg stores distance from startId to this nodeId, where the nodeId belong to Fi.O, and is initialed to be infinite
+// routeTable is created by func GenerateRouteTable
+// startID is the start id of global graph, usually we set it to node 1
+// returned bool value indicates which there has some message need to be send
+// the map value is the message need to be send
+// map[i] is a list of message need to be sent to partition i
+func SSSP_PEVal(g graph.Graph, distance map[graph.ID]int64, exchangeMsg map[graph.ID]int64, routeTable map[graph.ID][]*BoundMsg, startID graph.ID) (bool, map[int][]*Pair) {
+	nodes := g.GetNodes()
+
+	if _, ok := nodes[startID]; !ok {
+		return false, make(map[int][]*Pair)
+	}
+
+	FO := g.GetFOs()
+
+	updatedID := make([]graph.ID, 0)
+	pq := make(PriorityQueue, 0)
+
+	startPair := &Pair{
+		NodeId:   startID,
+		Distance: 0,
+	}
+	heap.Push(&pq, startPair)
+
+	for pq.Len() > 0 {
+		top := heap.Pop(&pq).(*Pair)
+		srcID := top.NodeId
+		nowDis := top.Distance
+
+		//fmt.Printf("srcID: %v  dis: %v\n", srcID, nowDis)
+
+		if nowDis >= distance[srcID] {
+			continue
+		}
+
+		distance[srcID] = nowDis
+		if msgs, ok := routeTable[srcID]; ok {
+			//fmt.Printf("srcId:%v\n", srcID)
+			for _, msg := range msgs {
+				//fmt.Printf("exchangeMsg[%v]: %v\n", msg.DstId, exchangeMsg[msg.DstId])
+				if exchangeMsg[msg.DstId] <= nowDis + msg.RouteLen {
+					continue
+				}
+				//fmt.Printf("UpdateId:%v\n", msg.DstId)
+				exchangeMsg[msg.DstId] = nowDis + msg.RouteLen
+				updatedID = append(updatedID, msg.DstId)
+			}
+		}
+
+		targets, _ := g.GetTargets(srcID)
+		for disID := range targets {
+			l, _ := g.GetWeight(srcID, disID)
+			weight := int64(l)
+			if distance[disID] > nowDis + weight {
+				heap.Push(&pq, &Pair{NodeId:disID, Distance:nowDis + weight,})
+			}
+		}
+	}
+
+	filterMap := make(map[graph.ID]bool)
+	messageMap := make(map[int][]*Pair)
+	for _, id := range updatedID {
+		if _, ok := filterMap[id]; ok {
+			continue
+		}
+		filterMap[id] = true
+
+		partition := FO[id][0].RoutePartition()
+		dis := exchangeMsg[id]
+		if _, ok := messageMap[partition]; !ok {
+			messageMap[partition] = make([]*Pair, 0)
+		}
+		messageMap[partition] = append(messageMap[partition], &Pair{NodeId:id, Distance:dis,})
+	}
+
+	return len(messageMap) != 0, messageMap
+}
+
+// the arguments is similar with PEVal
+// the only difference is updated, which is the message this partition received
+func SSSP_IncEval(g graph.Graph, distance map[graph.ID]int64, exchangeMsg map[graph.ID]int64, routeTable map[graph.ID][]*BoundMsg, updated []*Pair) (bool, map[int][]*Pair) {
+	//nodes := g.GetNodes()
+
+	if len(updated) == 0 {
+		return false, make(map[int][]*Pair)
+	}
+
+	FO := g.GetFOs()
+
+	updatedID := make([]graph.ID, 0)
+	pq := make(PriorityQueue, 0)
+
+	updated = SSSP_aggregateMsg(updated)
+
+	for _, ssspMsg := range updated {
+		startPair := &Pair{
+			NodeId:   ssspMsg.NodeId,
+			Distance: ssspMsg.Distance,
+		}
+		heap.Push(&pq, startPair)
+	}
+
+	for pq.Len() > 0 {
+		top := heap.Pop(&pq).(*Pair)
+		srcID := top.NodeId
+		nowDis := top.Distance
+
+		if nowDis >= distance[srcID] {
+			continue
+		}
+
+		distance[srcID] = nowDis
+		if msgs, ok := routeTable[srcID]; ok {
+			for _, msg := range msgs {
+				if exchangeMsg[msg.DstId] <= nowDis + msg.RouteLen {
+					continue
+				}
+				exchangeMsg[msg.DstId] = nowDis + msg.RouteLen
+				updatedID = append(updatedID, msg.DstId)
+			}
+		}
+
+		targets, _ := g.GetTargets(srcID)
+		for disID := range targets {
+			l, _ := g.GetWeight(srcID, disID)
+			weight := int64(l)
+			if distance[disID] > nowDis + weight {
+				heap.Push(&pq, &Pair{NodeId:disID, Distance:nowDis + weight,})
+			}
+		}
+	}
+
+	filterMap := make(map[graph.ID]bool)
+	messageMap := make(map[int][]*Pair)
+	for _, id := range updatedID {
+		if _, ok := filterMap[id]; ok {
+			continue
+		}
+		filterMap[id] = true
+
+		partition := FO[id][0].RoutePartition()
+		dis := exchangeMsg[id]
+		if _, ok := messageMap[partition]; !ok {
+			messageMap[partition] = make([]*Pair, 0)
+		}
+		messageMap[partition] = append(messageMap[partition], &Pair{NodeId:id, Distance:dis})
+	}
+
+	return len(messageMap) != 0, messageMap
+}
--- src/test.go
+++ src/test.go
@@ -2,42 +2,95 @@ package main
 
 import (
 	"fmt"
-	. "graph"
+	"algorithm"
+	"graph"
 	"os"
-	"strconv"
+	"math"
+	//"strconv"
+	//"unicode"
 )
 
+func Generate(g graph.Graph) (map[graph.ID]int64, map[graph.ID]int64) {
+	distance := make(map[graph.ID]int64)
+	exchangeMsg := make(map[graph.ID]int64)
+
+	for id := range g.GetNodes() {
+		distance[id] = math.MaxInt64
+	}
+
+	for id := range g.GetFOs() {
+		exchangeMsg[id] = math.MaxInt64
+	}
+	return distance, exchangeMsg
+}
+
+// This example creates a PriorityQueue with some items, adds and manipulates an item,
+// and then removes the items in priority order.
 func main() {
+	subgraphPath := "C:\\Users\\zpltys\\code\\GRAPE\\test_data\\subgraph.json"
+	partitionPath := "C:\\Users\\zpltys\\code\\GRAPE\\test_data\\partition.json"
 
-	subGraphDir := "C:\\Users\\zpltys\\code\\GRAPE\\test_data\\subgraph.json"
-	partitionDir := "C:\\Users\\zpltys\\code\\GRAPE\\test_data\\partition.json"
+	fmt.Println("start")
+	f0, _ := os.Open(subgraphPath)
+	pf0, _ := os.Open(partitionPath)
+	g0, _ := graph.NewGraphFromJSON(f0, pf0, "0")
+	f0.Close()
+	pf0.Close()
 
-	f, err := os.Open(subGraphDir)
-	//println(err)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	f1, _ := os.Open(subgraphPath)
+	pf1, _ := os.Open(partitionPath)
+	g1, _ := graph.NewGraphFromJSON(f1, pf1, "1")
+	f1.Close()
+	pf1.Close()
 
-	pf, err := os.Open(partitionDir)
-	g, err := NewGraphFromJSON(f, pf, "0")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(g.String())
+	dis0, exc0:= Generate(g0)
+	dis1, exc1 := Generate(g1)
 
-	for srcId, msg := range g.GetFIs() {
-		fmt.Println("FI: " + srcId.String())
-		for i := 0; i < len(msg); i++ {
-			fmt.Println("nodeId:" + msg[i].RelatedId().String() + "  partitionId:" + strconv.Itoa(msg[i].RoutePartition()))
+	routeTable0 := algorithm.GenerateRouteTable(g0.GetFOs())
+	routeTable1 := algorithm.GenerateRouteTable(g1.GetFOs())
+/*
+	for id, msgs := range routeTable1 {
+		for _, msg := range msgs {
+			fmt.Printf("routeTable1 : id:%v, disId:%v, routeLen:%v\n", id, msg.DstId, msg.RouteLen)
 		}
 	}
+*/
 
-	for srcId, msg := range g.GetFOs() {
-		fmt.Println("FO: " + srcId.String())
-		for i := 0; i < len(msg); i++ {
-			fmt.Println("nodeId:" + msg[i].RelatedId().String() + "  partitionId:" + strconv.Itoa(msg[i].RoutePartition()))
+	continue0, messageMap0 := algorithm.SSSP_PEVal(g0, dis0, exc0, routeTable0, graph.StringID("1"))
+	continue1, messageMap1 := algorithm.SSSP_PEVal(g1, dis1, exc1, routeTable1, graph.StringID("1"))
+
+	fmt.Printf("continue1:%v\n", continue1 )
+	var nc0, nc1 bool
+	var nmsg0, nmsg1 map[int][]*algorithm.Pair
+
+	for continue0 || continue1 {
+		fmt.Println("interation")
+		for i := 0; i <= 1; i++ {
+			message := make([]*algorithm.Pair, 0)
+			if m0, ok := messageMap0[i]; ok {
+				message = append(message, m0...)
+			}
+			if m1, ok := messageMap1[i]; ok {
+				message = append(message, m1...)
+			}
+
+			if i == 0 {
+				nc0, nmsg0 = algorithm.SSSP_IncEval(g0, dis0, exc0, routeTable0, message)
+			} else {
+				nc1, nmsg1 = algorithm.SSSP_IncEval(g1, dis1, exc1, routeTable1, message)
+			}
 		}
+		continue0 = nc0
+		continue1 = nc1
+		messageMap0 = nmsg0
+		messageMap1 = nmsg1
 	}
 
+
+	for id, dis := range dis0 {
+		fmt.Printf("g0:  %v : %v\n", id, dis)
+	}
+	for id, dis := range dis1 {
+		fmt.Printf("g1:  %v : %v\n", id, dis)
+	}
 }
