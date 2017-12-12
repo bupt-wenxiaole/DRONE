3ee82ab14b118b88b49b5e39dbfc2917e63c62af
zpltys <gzpltys@gmail.com>
2017-12-12 10:50:17 +0800

    init

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -1,155 +1,49 @@
 package worker
 
 import (
-    "log"
-    "net"
-    "time"
     pb "protobuf"
-    "golang.org/x/net/context"
     "google.golang.org/grpc"
-    "math/rand"
-    "strconv"
     "graph"
+    "sync"
+    "math"
 )
 
-type DistKV struct {
-    Node string
-    Distance int
-}
-type DistKVChanGRPC struct {
-    peerMessageChan chan DistKV 
-}
+func Generate(g graph.Graph) (map[graph.ID]int64, map[graph.ID]int64) {
+    distance := make(map[graph.ID]int64)
+    exchangeMsg := make(map[graph.ID]int64)
 
-type BoundMsg struct {
-    DstId graph.ID
-    RouteLen int64
-}
-
-func (s *DistKVChanGRPC) Put(ctx context.Context, r *pb.PutRequest) (*pb.PutResponse, error) {
-    //quote: 
-    //In Google : we require that Go programmers pass a Context parameter as the first argument 
-    //to every function on the call path between incoming and outgoing requests. 
-    resp := &pb.PutResponse{}
-    resp.Header = &pb.ResponseHeader{}
-    distance, err := strconv.Atoi(string(r.DistanceTosourceNode))
-    if err != nil {
-        resp.Header.Ok = true
-        return resp, err
+    for id := range g.GetNodes() {
+        distance[id] = math.MaxInt64
     }
-    var messageBlock DistKV = DistKV{Node:string(r.NodeIndex), Distance: distance}
-    s.peerMessageChan <- messageBlock
-    resp.Header.Ok = true
-    return resp, nil
+
+    for id := range g.GetFOs() {
+        exchangeMsg[id] = math.MaxInt64
+    }
+    return distance, exchangeMsg
 }
 
-func startServerGRPC(port string, peerNumber int) {
-    ln, err := net.Listen("tcp", port)
-    if err != nil {
-        panic(err)
-    }
+type Worker struct {
+    mutex *sync.Mutex
 
-    s := &DistKVChanGRPC{}
-    s.peerMessageChan = make(chan DistKV, peerNumber)
-    grpcServer := grpc.NewServer()
-    pb.RegisterDistKVServer(grpcServer, s)
-    go func() {
-        if err := grpcServer.Serve(ln); err != nil {
-            panic(err)
-        }
-    }()
+    workers []string
+    selfId int        // the id of this worker itself in workers
+    grpcHandlers []*grpc.ClientConn
+
+    g graph.Graph
+    distance map[graph.ID]int64
+    exchangeMsg map[graph.ID]int64
 }
 
-func Stress(port, endpoint string, keys, vals [][]byte, connsN, clientsN int) {
-    //connsN means number of tcp connection, clientsN means numbers of grpc client
-    go startServerGRPC(port, 100)
-
-    conns := make([]*grpc.ClientConn, connsN)
-    for i := range conns {
-        conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
-        if err != nil {
-            panic(err)
-        }
-        conns[i] = conn
-    }
-    clients := make([]pb.DistKVClient, clientsN)
-    for i := range clients {
-        clients[i] = pb.NewDistKVClient(conns[i%int(connsN)])
-    }
-
-    requests := make(chan *pb.PutRequest, len(keys))
-    done, errChan := make(chan struct{}), make(chan error)
-
-    for i := range clients {
-        go func(i int, requests chan *pb.PutRequest) {
-            var cnt = 0
-            for r := range requests {
-                if _, err := clients[i].Put(context.Background(), r); err != nil {
-                    errChan <- err
-                    return
-                }
-            }
-            done <- struct{}{}
-        }(i, requests)
-    }
-
-    st := time.Now()
-
-    for i := range keys {
-        r := &pb.PutRequest{
-            NodeIndex:   keys[i],
-            DistanceTosourceNode: vals[i],
-        }
-        requests <- r
-    }
-
-    close(requests)
-
-    cn := 0
-    for cn != len(clients) {
-        select {
-        case err := <-errChan:
-            panic(err)
-        case <-done:
-            cn++
-        }
-    }
-    close(done)
-    close(errChan)
-
-    tt := time.Since(st)
-    size := len(keys)
-    pt := tt / time.Duration(size)
-    log.Printf("GRPC took %v for %d requests with %d client(s) (%v per each).\n", tt, size, clientsN, pt)
+func (w *Worker) Lock() {
+    w.mutex.Lock()
 }
 
-func GenerateRouteTable(FO map[graph.ID][]graph.RouteMsg) map[graph.ID][]*BoundMsg {
-    routeTable := make(map[graph.ID][]*BoundMsg)
-    for fo, msgs := range FO {
-        for _, msg := range msgs {
-            srcId := msg.RelatedId()
-            if _, ok := routeTable[srcId]; !ok {
-                routeTable[srcId] = make([]*BoundMsg, 0)
-            }
-
-            nowMsg := &BoundMsg{
-                DstId:    fo,
-                RouteLen: int64(msg.RelatedWgt()),
-            }
-            routeTable[srcId] = append(routeTable[srcId], nowMsg)
-        }
-    }
-    return routeTable
+func (w * Worker) UnLock() {
+    w.mutex.Unlock()
 }
 
-func main() {
-    var keys, vals [][]byte
-    var pairNum = 3
-    for i := 0; i < pairNum; i++ {
-        var key = []byte(strconv.Itoa(rand.Intn(10)))
-        var val = []byte(strconv.Itoa(rand.Intn(100)))
-        keys = append(keys, key)
-        vals = append(vals, val)
-    }
-    Stress(":9000", "10.2.152.24:9000",keys, vals, 1, 10)
-
+func newWorker(name string) *Worker {
+    w := new(Worker)
+    w.mutex = new(sync.Mutex)
+    return w
 }
