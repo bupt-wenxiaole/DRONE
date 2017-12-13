f7795513303ab4d0b56ec90321b131b9c3fbb3e3
bupt-wenxiaole <xiaole.wen@bupt.edu.cn>
2017-12-13 20:57:38 +0800

    modify master.go's draft, make it no compile error, wait for inter with worker.go

--- src/master.go
+++ src/master.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"log"
-	"net"
-	pb "protobuf"
+	"bufio"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
+	"os"
+	pb "protobuf"
+	"strings"
 	"sync"
 	"tools"
-	"os"
-	"bufio"
-	"strings"
-	"io"
 )
 
 type Master struct {
-	mu *sync.Mutex
+	mu      *sync.Mutex
 	newCond *sync.Cond
 	address string
 	// signals when Register() adds to workers[]
 	// number of worker
-	workerNum  int
-	workersAddress [] string
+	workerNum      int
+	workersAddress []string
 	//master know worker's address from config.txt and worker's index
 	//TCP connection Reuse : after accept resgister then dial and save the handler
 	//https://github.com/grpc/grpc-go/issues/682
@@ -32,11 +32,11 @@ type Master struct {
 	// Name of Input File subgraph json
 	partitionFiles string
 	// Name of Input File partition json
-	shutdown chan struct{}
+	shutdown     chan struct{}
 	registerDone chan bool
-	statistic []int
+	statistic    []int
 	//each worker's statistic data
-	PhaseDoneChan chan bool
+	wg          *sync.WaitGroup
 	JobDoneChan chan bool
 }
 
@@ -47,6 +47,7 @@ func (mr *Master) Lock() {
 func (mr *Master) Unlock() {
 	mr.mu.Unlock()
 }
+
 // Register is an RPC method that is called by workers after they have started
 // up to report that they are ready to receive tasks.
 // Locks for multiple worker concurrently access worker list
@@ -55,22 +56,21 @@ func (mr *Master) Register(ctx context.Context, args *pb.RegisterRequest) (r *pb
 	defer mr.Unlock()
 	log.Printf("Register: worker %d\n", args.WorkerIndex)
 	endpoint := mr.workersAddress[args.WorkerIndex]
-    conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
-    if err != nil {
-        panic(err)
-    }
-    if _, ok := mr.grpcHandlers[args.WorkerIndex]; ok {
-    	//TODO: if the master terminate, how about worker's register?
-		log.Fatal("%d worker register more than one times", args.WorkerIndex)
+	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
 	}
-	else {
+	if _, ok := mr.grpcHandlers[args.WorkerIndex]; ok {
+		//TODO: if the master terminate, how about worker's register?
+		log.Fatal("%d worker register more than one times", args.WorkerIndex)
+	} else {
 		mr.grpcHandlers[args.WorkerIndex] = conn
 	}
-	if len(mr.grpcHandlers)  == mr.workerNum {
+	if len(mr.grpcHandlers) == mr.workerNum {
 		mr.registerDone <- true
 	}
 	// There is no need about scheduler
-	return &pb.RegisterResponse{Ok:true}, nil
+	return &pb.RegisterResponse{Ok: true}, nil
 }
 
 // newMaster initializes a new Master
@@ -81,7 +81,7 @@ func newMaster() (mr *Master) {
 	mr.newCond = sync.NewCond(mr.mu)
 	mr.JobDoneChan = make(chan bool)
 	mr.registerDone = make(chan bool)
-	mr.PhaseDoneChan = make(chan bool)
+	mr.wg = new(sync.WaitGroup)
 	//workersAddress slice's index is worker's Index
 	//read from Config text
 	mr.workersAddress = make([]string, mr.workerNum)
@@ -107,8 +107,7 @@ func (mr *Master) ReadConfig() {
 		if first {
 			mr.address = conf[1]
 			first = false
-		}
-		else {
+		} else {
 			mr.workersAddress = append(mr.workersAddres, conf[1])
 		}
 	}
@@ -119,7 +118,7 @@ func (mr *Master) wait() {
 func (mr *Master) KillWorkers() {
 	mr.Lock()
 	defer mr.Unlock()
-	for i:= 1; i <= mr.workerNum; i++ {
+	for i := 1; i <= mr.workerNum; i++ {
 		log.Printf("Master: shutdown worker %d\n", i)
 		handler := mr.grpcHandlers[int32(i)]
 		client := pb.NewWorkerClient(handler)
@@ -149,27 +148,21 @@ func (mr *Master) StartMasterServer() {
 
 }
 func (mr *Master) PEval() bool {
-	cnt := 0
-	for i:= 1; i <= mr.workerNum; i++{
+	for i := 1; i <= mr.workerNum; i++ {
 		log.Printf("Master: start %d PEval", i)
+		mr.wg.Add(1)
 		go func() {
+			defer mr.wg.Done()
 			handler := mr.grpcHandlers[int32(i)]
 			client := pb.NewWorkerClient(handler)
-			}
-			if reply, err := client.PEval(context.Background(), pevalrequset), err != nil {
-				log.Fatal("Fail to execute PEval %s", w)
-			} else {
-			mr.lock()
-			cnt++
-			if cnt == mr.workerNum{
-			mr.PEvalDone <- true
-
-			}
-			mr.unlock()
+			pevalRequest := pb.PEvalRequest{}
+			if _, err := client.PEval(context.Background(), pevalRequest); err != nil {
+				log.Fatal("Fail to execute PEval %d", i)
+				//TODO: still something todo: Master Just terminate, how about the Worker
 			}
 		}()
 	}
-	<-mr.PEvalDone
+	mr.wg.Wait()
 	return true
 }
 
@@ -180,58 +173,50 @@ func (mr *Master) IncEvalALL() bool {
 		stepCount++
 		cnt := 0
 		update := false
-		for i, w := range mr.workers {
-			log.Printf("Master: start the %dth PEval of %s", stepCount, w)
+		for i := 1; i <= mr.workerNum; i++ {
+			log.Printf("Master: start the %dth PEval of worker %i", stepCount, i)
+			mr.wg.Add(1)
 			go func() {
-				handler := mr.grpcHandlers[i]
-				client := pb.Newwoerker_servcieClient(handler)
-				if reply, err := client.IncEval(context.Background(), &struct{}), err != nil {
-					log.Fatal("Fail to execute IncEval %s", w)
-				}
-				else {
-					mr.lock()
-					cnt ++
+				defer mr.wg.Done()
+				handler := mr.grpcHandlers[int32(i)]
+				client := pb.NewWorkerClient(handler)
+				incEvalRequest := &pb.IncEvalRequest{}
+				if reply, err := client.IncEval(context.Background(), incEvalRequest); err != nil {
+					log.Fatal("Fail to execute IncEval worker %d", i)
+				} else {
+					mr.Lock()
+					//multiple goroutines access update
 					update = update || reply.update
-					if cnt == mr.workerNum {
-						mr.IncEvalDone <- true
-					}
-					mr.unlock()
+					mr.Unlock()
 				}
 			}()
 		}
-		<- mr.IncEvalDone
+		mr.wg.Wait()
 		if update == false {
 			break
 		}
 	}
 	return true
 }
-func (mr *Master) Assemble ok bool{
-	cnt := 0
-	for i, w := range mr.workers {
-		log.Printf("Master: start %s Assemble", w)
+func (mr *Master) Assemble() bool {
+	for i := 1; i <= mr.workerNum; i++ {
+		log.Printf("Master: start worker %d Assemble", i)
+		mr.wg.Add(1)
 		go func() {
-			handler := mr.grpcHandlers[i]
-			client := pb.Newwoerker_servcieClient(handler)
-			if reply, err := client.Assemble(context.Background(), &struct{}), err != nil {
-				log.Fatal("Fail to execute Assemble %s", w)
-			}
-			else {
-				mr.lock()
-				cnt++
-				if cnt == mr.workerNum{
-					mr.AssembleDone <- true
-
-				}
-				mr.unlock()
+			defer mr.wg.Done()
+			handler := mr.grpcHandlers[int32(i)]
+			client := pb.NewWorkerClient(handler)
+			assembleRequest := &pb.AssembleRequest{}
+			if reply, err := client.Assemble(context.Background(), assembleRequest); err != nil {
+				log.Fatal("Fail to execute Assemble worker %d", i)
 			}
 		}()
 	}
-	<- mr.AssembleDone
+	mr.wg.Wait()
 	return true
 }
 func (mr *Master) StopRPCServer() {
-
+	return
 }
 
 func RunJob(jobName string, subGraphJson string, partitionJson string) {
