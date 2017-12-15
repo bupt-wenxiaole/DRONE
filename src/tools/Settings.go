de32560e9506147b70edb07847961046ff40a326
bupt-wenxiaole <xiaole.wen@bupt.edu.cn>
2017-12-15 18:36:50 +0800

    modify config

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -3,8 +3,8 @@ package tools
 const (
 	AlluxioHost   = "10.2.152.24"
 	ResultPath    = "/GRAPE/result/"
-	GraphPath     = "/GRAPE/data/subgraph/"
-	PartitionPath = "/GRAPE/data/partition/"
+	GraphPath     = "/GRAPE/data/"
+	PartitionPath = "/GRAPE/data/"
 	ConfigPath = "../test_data/config.txt"
 
 	maxBufferSize = 1024000
