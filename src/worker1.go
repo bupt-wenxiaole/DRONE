713b7c4c314c1fd309a8400865fe5e8bf3e6d1d8
bupt-wenxiaole <xiaole.wen@bupt.edu.cn>
2017-12-15 18:49:25 +0800

    modify worker1.go

--- src/worker1.go
+++ src/worker1.go
@@ -3,10 +3,16 @@ package main
 import (
 	"worker"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("start")
-	worker.RunWorker(1)
+	workerID, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("conv fail!")
+	}
+	worker.RunWorker(workerID)
 	fmt.Println("stop")
 }
