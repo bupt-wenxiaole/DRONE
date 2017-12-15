5b00387c074077d8a00ccd8aba13a99c3ec719a3
bupt-wenxiaole <xiaole.wen@bupt.edu.cn>
2017-12-15 19:00:19 +0800

    debug for alluxio path

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -97,8 +97,10 @@ func (w *Worker) PEval(ctx context.Context, args *pb.PEvalRequest) (*pb.PEvalRes
 
 	// Load graph data
 	fs := tools.GenerateAlluxioClient(tools.AlluxioHost)
+	log.Print("load path %s\n", tools.GraphPath + "G" + strconv.Itoa(w.selfId - 1) + ".json")
 	graphIO, _ := tools.ReadFromAlluxio(fs, tools.GraphPath + "G" + strconv.Itoa(w.selfId - 1) + ".json")
 	defer graphIO.Close()
+	log.Print("load path %s\n", tools.PartitionPath + "P" + strconv.Itoa(w.selfId - 1) + ".json")
 	partitionIO, _ := tools.ReadFromAlluxio(fs, tools.PartitionPath + "P" + strconv.Itoa(w.selfId - 1) + ".json")
 	defer partitionIO.Close()
 	w.g, _ = graph.NewGraphFromJSON(graphIO, partitionIO, strconv.Itoa(w.selfId - 1))
