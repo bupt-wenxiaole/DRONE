4ac22cffceb2f716bb26b229b5436e06f80c82a1
bupt-wenxiaole <506539136@qq.com>
2017-12-18 16:05:31 +0800

    delelte ^m in scripts

--- sbin/start-all.sh
+++ sbin/start-all.sh
@@ -3,13 +3,13 @@
 # set the IP of slaves and the ssh user name
 partitionNum="4"
 slaveIp1="10.2.152.24"
-slaveID1= "1"
+slaveID1="1"
 slaveIp2="10.2.152.22"
-slaveID2= "2"
+slaveID2="2"
 slaveIp3="10.2.152.21"
-slaveID3= "3"
+slaveID3="3"
 slaveIp4="10.2.152.23"
-slaveID4= "4"
+slaveID4="4"
 slaveName=$(whoami)
 
 # set the golang working directory containing the code
@@ -22,18 +22,18 @@ masterPath=$GOPATH
 cd ${masterPath}/src/
 
 # start the master daemon and return immediately
-
-/usr/local/go/bin/go run master.go
+pwd
+#/usr/local/go/bin/go run master.go
 
 # start shell script to start the worker daemon
-ssh ${slaveName}@${slaveIp1} "chmod +x ${slavePath}/sbin/startWorker.sh"
-ssh ${slaveName}@${slaveIp1} "${slavePath}/sbin/startWorker.sh ${slaveID1} ${partitionNum} &"
+ssh ${slaveName}@${slaveIp1} "chmod +x ${slavePath}/sbin/start-worker.sh"
+ssh ${slaveName}@${slaveIp1} "${slavePath}/sbin/start-worker.sh ${slaveID1} ${partitionNum}" > log.txt
 
-ssh ${slaveName}@${slaveIp2} "chmod +x ${slavePath}/sbin/startWorker.sh"
-ssh ${slaveName}@${slaveIp2} "${slavePath}/sbin/startWorker.sh ${slaveID2} ${partitionNum} &"
+ssh ${slaveName}@${slaveIp2} "chmod +x ${slavePath}/sbin/start-worker.sh"
+ssh ${slaveName}@${slaveIp2} "${slavePath}/sbin/start-worker.sh ${slaveID2} ${partitionNum} &"
 
-ssh ${slaveName}@${slaveIp3} "chmod +x ${slavePath}/sbin/startWorker.sh"
-ssh ${slaveName}@${slaveIp3} "${slavePath}/sbin/startWorker.sh ${slaveID3} ${partitionNum} &"
+ssh ${slaveName}@${slaveIp3} "chmod +x ${slavePath}/sbin/start-worker.sh"
+ssh ${slaveName}@${slaveIp3} "${slavePath}/sbin/start-worker.sh ${slaveID3} ${partitionNum} &"
 
-ssh ${slaveName}@${slaveIp4} "chmod +x ${slavePath}/sbin/startWorker.sh"
-ssh ${slaveName}@${slaveIp4} "${slavePath}/sbin/startWorker.sh ${slaveID4} ${partitionNum} &"
+ssh ${slaveName}@${slaveIp4} "chmod +x ${slavePath}/sbin/start-worker.sh"
+ssh ${slaveName}@${slaveIp4} "${slavePath}/sbin/start-worker.sh ${slaveID4} ${partitionNum} &"
