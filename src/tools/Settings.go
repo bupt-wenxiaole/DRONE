86172cac71bc0ab0007598e49f796f2ede176031
zpltys <gzpltys@gmail.com>
2017-12-30 14:02:32 +0800

    change config

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -9,7 +9,7 @@ const (
 	ConfigPath = "../test_data/config.txt"
 
 	PatternPath = "../test_data/pattern.txt"
-	GraphSimulationTypeModel = 100
+	GraphSimulationTypeModel = 8
 
 	RPCSendSize = 10000
 
