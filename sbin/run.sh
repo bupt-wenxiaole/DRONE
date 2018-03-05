90b41e2f328f2b8f880f076467433040612898c1
zpltys <gzpltys@gmail.com>
2018-03-05 15:36:20 +0800

    remove file

--- sbin/run.sh
+++ sbin/run.sh
@@ -1,5 +1,5 @@
 for ((i=$1; i<=$2;++i))
 do
     rm $i.out
-    nohup /usr/local/go/bin/go run ../src/worker/SimWorker.go $i $3 > $i.out &
+    nohup /usr/local/go/bin/go run ../src/worker2.go $i $3 > $i.out &
 done
