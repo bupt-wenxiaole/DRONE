2bcbb23dbaa7a9004b4d622966f97aa6aa9f787d
zpltys <gzpltys@gmail.com>
2018-03-19 17:21:41 +0800

    delete file

--- src/run.sh
+++
@@ -1,6 +0,0 @@
-ps -aux | grep go-build | awk '{print $2}' | xargs kill -9
-nohup /usr/local/go/bin/go run worker2.go 1 16 > 1.out &
-nohup /usr/local/go/bin/go run worker2.go 2 16 > 2.out &
-nohup /usr/local/go/bin/go run worker2.go 3 16 > 3.out &
-nohup /usr/local/go/bin/go run worker2.go 4 16 > 4.out &
-#nohup /usr/local/go/bin/go run worker2.go 5 20 > 5.out &
