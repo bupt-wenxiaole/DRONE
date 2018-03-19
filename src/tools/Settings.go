e17cf18f366c94148b2e81e9444a0cd9e28744d1
zpltys <gzpltys@gmail.com>
2018-03-19 16:59:04 +0800

    merge conflict

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -1,7 +1,6 @@
 package tools
 
 const (
-<<<<<<< HEAD
 	ResultPath    = "/mnt/nfs/zpltys/GRAPE_DATA/result/"
 	//NFSPath       = "/mnt/nfs/zpltys/GRAPE_DATA/liveJournal/"
 	//NFSPath = "/home/acgict/inputgraph/node"
@@ -10,15 +9,8 @@ const (
 
 	LoadFromJson = true
 
-=======
-	AlluxioHost   = "10.2.152.24"
-	ResultPath    = "/GRAPE/result/"
-	GraphPath     = "/GRAPE/simData/"
-	PartitionPath = "/GRAPE/simData/"
 	//NFSPath = "/home/acgict/inputgraph/"
-	NFSPath = "/home/zpltys/code/GRAPE/16p/"
-	ReadFromTxt   = true
->>>>>>> acefdd1feb4bdc1f91bbc4791123d7a8f57d98bd
+
 	//LocalTempFile = "temp.json"
 	ConfigPath = "../test_data/config.txt"
 	//ConfigPath = "config.txt"
