ca6b0d2060745b2a9e14c48c736149071067515b
zpltys <gzpltys@gmail.com>
2018-03-20 22:24:19 +0800

    change config

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -5,17 +5,18 @@ const (
 	//NFSPath = "/home/acgict/inputgraph/node"
 	//NFSPath = "/home/zpltys/code/GRAPE/16p/node"
 	//NFSPath = "/mnt/nfs/zpltys/GRAPE_DATA/liveJournal/"
-	NFSPath = "/mnt/nfs/zpltys/GRAPE_DATA/USARoad/"
+	//NFSPath = "/mnt/nfs/zpltys/GRAPE_DATA/USARoad/"
+	NFSPath = "/mnt/nfs/xwen/generate_graph/"
 
-	LoadFromJson = true
+	LoadFromJson = false
 
 	//NFSPath = "/home/acgict/inputgraph/"
 
 	//ConfigPath = "../test_data/config.txt"
 	ConfigPath = "config.txt"
 
-	PatternPath = "../test_data/pattern.txt"
-	//PatternPath = "pattern.txt"
+	//PatternPath = "../test_data/pattern.txt"
+	PatternPath = "pattern.txt"
 	GraphSimulationTypeModel = 100
 
 	RPCSendSize = 100000
