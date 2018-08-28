b1f3ee20ec7cb5b2722cb948721ca0f80ef51943
zpltys <gzpltys@gmail.com>
2018-08-28 10:50:07 +0800

    change path for pagerank

--- src/tools/Settings.go
+++ src/tools/Settings.go
@@ -2,14 +2,14 @@ package tools
 
 const (
 	//ResultPath = "/mnt/nfs/zpltys/test/"
-	//ResultPath = "/home/acgict/graph"
-	ResultPath = "/mnt/nfs/xwen/liveJournal_vc_part"
+	ResultPath = "/home/acgict/graph"
+	//ResultPath = "/mnt/nfs/xwen/liveJournal_vc_part"
 	//ResultPath = "/mnt/nfs/xwen/generate_graph/"
 	//ResultPath = "/mnt/nfs/xwen/graphData/"
 	//NFSPath = "/home/acgict/inputgraph/"
-	NFSPath = "/mnt/nfs/xwen/graphData/"
+	//NFSPath = "/mnt/nfs/xwen/graphData/"
 	//NFSPath = "/mnt/nfs/xwen/generate_graph/"
-	//NFSPath = "/mnt/nfs/xwen/liveJournal_vc_part"
+	NFSPath = "/mnt/nfs/xwen/liveJournal_vc_part"
 	WorkerOnSC = true
 
 	//ConfigPath = "../test_data/config.txt"
